ddbc6a1c68a5386ed9739ffc6f588e8ca5db8131
Gaurab Aryal <gaurabaryal94@gmail.com>
2026-02-21 22:50:00 +0545

    added a active-nav function for nav toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menuBtn = document.getElementById("menuBtn");
-    const navMenu = document.getElementById("navmenu");
-    
-    if (menuBtn && navMenu) {
-        menuBtn.addEventListener("click", () => {
-            navMenu.classList.toggle("active");
-        });
+  const menuBtn = document.getElementById("menuBtn");
+  const navMenu = document.getElementById("navmenu");
 
-        // Close menu when clicking a link
-        const navLinks = navMenu.querySelectorAll("a");
-        navLinks.forEach(link => {
-            link.addEventListener("click", () => {
-                navMenu.classList.remove("active");
-            });
-        });
-    }
+  if (menuBtn && navMenu) {
+    menuBtn.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+    });
+
+    // Close menu when clicking a link
+    const navLinks = navMenu.querySelectorAll("a");
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+
+      });
+    });
+  }
 });
 
-const lazyElements = document.querySelectorAll(".lazy-bg");
+const navlist = document.querySelectorAll('.link');
 
-const lazyObserver = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const el = entry.target;
-        const bg = el.dataset.bg;
+navlist.forEach(link => {
+  link.addEventListener('click', () => {
 
-        if (bg) {
-          el.style.backgroundImage = `url(${bg})`;
-          el.classList.add("loaded");
-          observer.unobserve(el);
-        }
-      }
+    navlist.forEach(item=>{
+      item.classList.remove('active-nav');
     });
-  },
-  {
-    rootMargin: "200px",
-    threshold: 0.1
-  }
-);
 
-lazyElements.forEach(el => lazyObserver.observe(el));
+    link.classList.add('active-nav');
+
+  });
+});
